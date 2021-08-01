454d152a3107cea91534a71372bc24270caa8b1c
AnthonyLzq <sluzquinosa@uni.pe>
2021-08-01 07:38:58 -0500

    feat: added copy webpack plugin to copy files from src to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 /**
  * @type {import('webpack').Configuration}
@@ -38,6 +39,14 @@ module.exports = {
     }),
     new MiniCSSExtractPlugin({
       filename: './index.css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images'
+        }
+      ]
     })
   ]
 }
