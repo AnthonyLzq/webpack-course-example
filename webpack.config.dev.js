6f102bac7a2c5278f0ae851676148121cefbb10e
AnthonyLzq <sluzquinosa@uni.pe>
2021-08-01 12:10:51 -0500

    feat: implemented webpack config in production mode and fixed duplicated files and cleaning dist folder in every build

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const DotenvPlugin = require('dotenv-webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 /**
  * @type {import('webpack').Configuration}
@@ -14,7 +16,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    assetModuleFilename: 'assets/images/[hash][ext][query]'
+    assetModuleFilename: 'assets/images/[name][ext]'
   },
   mode: 'development',
   resolve: {
@@ -23,7 +25,7 @@ module.exports = {
       '@utils': path.resolve(__dirname, './src/utils'),
       '@templates': path.resolve(__dirname, './src/templates'),
       '@styles': path.resolve(__dirname, './src/styles'),
-      '@images': path.resolve(__dirname, './src/assets/images'),
+      '@images': path.resolve(__dirname, './src/assets/images')
     }
   },
   module: {
@@ -55,7 +57,8 @@ module.exports = {
             publicPath: './fonts/',
             esModule: false
           }
-        }
+        },
+        type: 'javascript/auto'
       }
     ]
   },
@@ -69,14 +72,16 @@ module.exports = {
     new MiniCSSExtractPlugin({
       filename: './assets/[name].css'
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src', 'assets/images'),
-          to: 'assets/images'
-        }
-      ]
-    }),
-    new DotenvPlugin()
-  ]
+    new DotenvPlugin(),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: true
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CSSMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
 }
